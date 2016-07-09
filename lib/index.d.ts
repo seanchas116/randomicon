49e277fa1ed92c44539af7e3f33907dd547a2d08
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-09 20:01:42 +0900

    Add options

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,18 @@
 declare class IconGenerator {
     protected opts: {
         size?: number;
+        saturation?: number;
+        alpha?: number;
     };
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
     size: number;
+    saturation: number;
+    alpha: number;
     constructor(opts?: {
         size?: number;
+        saturation?: number;
+        alpha?: number;
     });
     generate(seed: string): void;
     toDataURL(): string;
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,13 @@ class IconGenerator {
   canvas: HTMLCanvasElement
   context: CanvasRenderingContext2D
   size = this.opts.size || 512
+  saturation = this.opts.saturation || 1
+  alpha = this.opts.alpha || 0.75
 
   constructor(protected opts: {
-    size?: number
+    size?: number,
+    saturation?: number,
+    alpha?: number,
   } = {}) {
     this.canvas = document.createElement("canvas")
     this.canvas.width = this.canvas.height = this.size
@@ -25,7 +29,7 @@ class IconGenerator {
       const x = rngRange(rng, -0.25, 1.25) * size
       const y = rngRange(rng, -0.25, 1.25) * size
       const radius = size / 2 * (1 + rng())
-      const color = tinycolor({h: rng() * 360, s: 1, v: 1, a: 0.75}).toRgbString()
+      const color = tinycolor({h: rng() * 360, s: this.saturation, v: 1, a: this.alpha}).toRgbString()
       context.fillStyle = color
       context.beginPath()
       context.arc(x, y, radius, 0, 2 * Math.PI)

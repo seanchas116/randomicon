c6a70fdc10bf4f3b790d4c49dd94362022b09d2e
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-09 19:42:30 +0900

    Generate image

--- src/example.ts
+++ src/example.ts
@@ -3,5 +3,7 @@ import IconGenerator = require("./index")
 for (const name of ["poe", "nyan", "hoge", "seanchas116"]) {
   const generator = new IconGenerator()
   generator.generate(name)
-  document.body.appendChild(generator.canvas)
+  const img = document.createElement("img")
+  img.src = generator.canvas.toDataURL()
+  document.body.appendChild(img)
 }
